c87563f229095a18eafaf9ed9e5fb60d39329e63
ChrisHolcomb <christopher.holcomb@gmail.com>
2021-12-19 20:54:47 -0600

    Added the Discount GRPC Code

---
+++ src/Services/Discount/Discount.GRPC/Extensions/HostExtensions.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Routing.Matching;
+using Npgsql;
+
+namespace Discount.GRPC.Extensions;
+
+public static class HostExtensions
+{
+    public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+    {
+        int retryForAvailability = retry.Value;
+
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var configuration = services.GetRequiredService<IConfiguration>();
+            var logger = services.GetRequiredService<ILogger<TContext>>();
+
+            try
+            {
+                logger.LogInformation("Migrating postgresql database. ");
+                
+                using var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                connection.Open();
+
+                using var command = new NpgsqlCommand
+                {
+                    Connection = connection
+                };
+
+                command.CommandText = "DROP TABLE IF EXISTS Coupon";
+                command.ExecuteNonQuery();
+
+                command.CommandText = @"CREATE TABLE Coupon(Id SERIAL PRIMARY KEY,
+                                                            ProductName VARCHAR(24) NOT NULL,
+                                                            Description TEXT,
+                                                            Amount INT)";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('iPhone X', 'iPhone Discount', 150);";
+                command.ExecuteNonQuery();
+                
+                command.CommandText = "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+                command.ExecuteNonQuery();
+                
+                logger.LogInformation("Migrated postgresql database. ");
+            }
+            catch (NpgsqlException e)
+            {
+                logger.LogError(e, "An error occured while migrating the postgresql database");
+
+                if (retryForAvailability < 50)
+                {
+                    retryForAvailability++;
+                    System.Threading.Thread.Sleep(2000);
+                    MigrateDatabase<TContext>(host, retryForAvailability);
+                }
+            }
+        }
+
+        return host;
+    }
+}
---
+++ src/Services/Discount/Discount.GRPC/Services/DiscountService.cs
@@ -0,0 +1,68 @@
+using AutoMapper;
+using Dapper;
+using Discount.GRPC.Entities;
+using Discount.GRPC.Protos;
+using Discount.GRPC.Repositories;
+using Grpc.Core;
+
+namespace Discount.GRPC.Services;
+
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    private readonly IDiscountRepository _repository;
+    private readonly IMapper _mapper;
+    private readonly ILogger<DiscountService> _logger;
+
+    public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger, IMapper mapper)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _mapper = mapper;
+    }
+
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await _repository.GetDiscount(request.ProductName);
+        if (coupon == null)
+        {
+            throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName}"));
+        }
+        _logger.LogInformation("Discount is retrieved for ProductName : {ProductName}, Amount : {Amount}", coupon.ProductName, coupon.Amount);
+        
+        var couponModel = _mapper.Map<CouponModel>(coupon); 
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = _mapper.Map<Coupon>(request.Coupon);
+
+        await _repository.CreateDiscount(coupon);
+        _logger.LogInformation("Discount is successfully created. ProductName : {ProductName}", coupon.ProductName);
+
+        var couponModel = _mapper.Map<CouponModel>(coupon);
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = _mapper.Map<Coupon>(request.Coupon);
+
+        await _repository.UpdateDiscount(coupon);
+        _logger.LogInformation("Discount is successfully updated. ProductName : {ProductName}", coupon.ProductName);
+
+        var couponModel = _mapper.Map<CouponModel>(coupon);
+        return couponModel;
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var deleted = await _repository.DeleteDiscount(request.ProductName);
+        var response = new DeleteDiscountResponse
+        {
+            Success = deleted
+        };
+
+        return response;
+    }
+}

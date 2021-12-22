4cabe816304ed84f853044efea3f4d9958be356a
ChrisHolcomb <christopher.holcomb@gmail.com>
2021-12-22 00:30:21 -0600

    Ocelot API Gateway implemented

--- src/ApiGateways/OcelotApiGateway/Program.cs
+++ src/ApiGateways/OcelotApiGateway/Program.cs
@@ -1,11 +1,21 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+    .AddCacheManager(settings =>
+    {
+        settings.WithDictionaryHandle();
+    });
 
-var app = builder.Build();
+
+// Choose the correct Ocelot config file based on the environment
+builder.WebHost.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+});
 
 // Configure Logging
 builder.WebHost.ConfigureLogging((hostingContext, loggingBuilder) =>
@@ -14,6 +24,9 @@ builder.WebHost.ConfigureLogging((hostingContext, loggingBuilder) =>
     loggingBuilder.AddConsole();
     loggingBuilder.AddDebug();
 });
+
+var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 

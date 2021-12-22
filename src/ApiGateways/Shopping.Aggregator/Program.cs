ca741859f9e077a6411e49888f9ede448bf277a8
ChrisHolcomb <christopher.holcomb@gmail.com>
2021-12-22 14:49:49 -0600

    Added shopping aggregator and put in docker

---
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -0,0 +1,36 @@
+using Shopping.Aggregator.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Typed HTTP Client configuration
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:CatalogUrl"]));
+builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:BasketUrl"]));
+builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
+    c.BaseAddress = new Uri(builder.Configuration["ApiSettings:OrderingUrl"]));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
